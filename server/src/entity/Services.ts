ac0b7b5aecb89611b26a2e74da5560545e56e59c
vikas583 <aroravikas583@gmail.com>
2022-11-08 01:01:06 +0530

    Service master data added

--- server/src/entity/Services.ts
+++ server/src/entity/Services.ts
@@ -1,18 +1,18 @@
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity()
-export class Service{
+export class Service {
     @PrimaryGeneratedColumn()
-    readonly serviceId : number;
+    readonly serviceId: number;
 
-    @Column({nullable : false, unique : true})
-    serviceName : string;
+    @Column({ nullable: false, unique: true })
+    serviceName: string;
 
-    @Column({nullable : false, default : 1})
-    isActive : number;
+    @Column({ nullable: false, default: 1 })
+    isActive: number;
 
-    @Column({nullable : false, default : "0"})
-    price : string;
+    @Column({ nullable: false, default: "0" })
+    price: string;
 
     @UpdateDateColumn({ nullable: true })
     updatedAt?: Date;
